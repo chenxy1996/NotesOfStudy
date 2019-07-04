2ebcb9ad434af0db7d1975f53d119b1cf3151860
Chen Xiangyu <www.459971074@qq.com>
2019-07-04 21:22:47 +0800

    20190704

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ To record study progress of daily life.
 
 ## Script *transferFormat.py*
 The python script is used to transfer the markdown file format of *YoudaoYun* to the format setted in *Typora* or vice versa.
+
+Python脚本*transferFormat.py*用来实现有道云markdown文件和Typora的markdown文件之间的互相转换.
---
+++ trasferFormat.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Transfer:
+    def __init__(self, path):
+        self.path = path
+    
+    def traverse(self, path): # Traverse the target path to find markdown files.
+        pass
+    
+    def youdToTypo(self, file_name): # Convert the format of md in YoudaoYun to that in Typora.
+        pass
+    
+    def typoToYoud(self, file_name): # Convert the format of md in Typora to that in YoudaoYun.
+        pass
+    
+        
