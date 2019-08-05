96826dd6d45ba664fe65cbc62a78de145fe3f186
Chen Xiangyu <www.459971074@qq.com>
2019-08-05 23:07:19 +0800

    20190805

--- javaScript/node.js
+++ javaScript/node.js
@@ -3,8 +3,9 @@ var funcs = [];
 // throws an error after one iteration
 for (let i = 0; i < 10; i++) {
     funcs.push(function() {
-        console.log(i, j);
+        console.log(i);
     });
 }
 
-funcs.forEach((eachFunc) => eachFunc());
+// funcs.forEach((eachFunc) => eachFunc());
+Array.prototype.forEach.call(funcs, (eachFunc) => eachFunc());
