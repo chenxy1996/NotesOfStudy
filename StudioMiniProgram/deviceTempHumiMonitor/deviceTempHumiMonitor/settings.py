5c71e7bc1581af75df6d35e682c814814da03942
Chen Xiangyu <www.459971074@qq.com>
2019-08-29 00:23:31 +0800

    create static directory

--- StudioMiniProgram/deviceTempHumiMonitor/deviceTempHumiMonitor/settings.py
+++ StudioMiniProgram/deviceTempHumiMonitor/deviceTempHumiMonitor/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
