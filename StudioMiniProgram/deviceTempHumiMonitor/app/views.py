c375342d8daf5b842ec032dd3a0030933453e6d7
Chen Xiangyu <www.459971074@qq.com>
2019-08-29 00:11:08 +0800

    添加管理员

--- StudioMiniProgram/deviceTempHumiMonitor/app/views.py
+++ StudioMiniProgram/deviceTempHumiMonitor/app/views.py
@@ -5,13 +5,17 @@ import sys
 sys.path.append("..")
 
 from my_util.test_util import *
+from configure import *
 
 
+'''
+Mimicking_Info_Sender： 模拟从数据库中得到 http request 需要的数据并发送 json 数据对象
+'''
 class Mimicking_Info_Sender(View):
     def get(self, request, num):
         return_json_data = {}
 
-        if num <= 300:
+        if num <= MAX_DATA_REQUEST_NUMBER:
             return_json_data["data"] = get_arbitary_data_list(num)
             return_json_data["dataNum"] = num
         else:
---
+++ StudioMiniProgram/deviceTempHumiMonitor/configure.py
@@ -0,0 +1,2 @@
+# 所能请求的最多温湿度信息数量
+MAX_DATA_REQUEST_NUMBER = 500
