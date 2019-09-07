8a7f45cbee94bf552dee8c924b0e6c1431364449
Chen Xiangyu <chenxy19960202@gmail.com>
2019-09-07 23:28:01 +0800

    add map location function

--- StudioMiniProgram/miniProgramTempHumi/pages/map/map.js
+++ StudioMiniProgram/miniProgramTempHumi/pages/map/map.js
@@ -1,85 +1,123 @@
-// 引用百度地图微信小程序JSAPI模块 
-var bmap = require('../../libs/bmap-wx.min.js');
-var wxMarkerData = [];
+// pages/map/map.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    markers: [],
-    latitude: '',
-    longitude: '',
-    placeData: {}
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+      id: 1,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      name: 'T.I.T 创意园'
+    }],
+    covers: [{
+      latitude: 23.099994,
+      longitude: 113.344520,
+      iconPath: '/static/image/location.png'
+    }, {
+      latitude: 23.099994,
+      longitude: 113.304520,
+      iconPath: '/static/image/location.png'
+    }]
   },
-  makertap: function (e) {
-    var that = this;
-    var id = e.markerId;
+  
 
-    // test
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
-    console.log(e)
-    
-    that.showSearchInfo(wxMarkerData, id);
-    that.changeMarkerColor(wxMarkerData, id);
-  },
-  onLoad: function () {
-    var that = this;
-    // 新建百度地图对象 
-    var BMap = new bmap.BMapWX({
-      ak: 'fGQ4kNDzwmwHKDq0GmTDlpH3wRiavb0h'
-    });
-    var fail = function (data) {
-      console.log(data)
-    };
-    var success = function (data) {
-      wxMarkerData = data.wxMarkerData;
-      // 测试用
-      console.log(data);
-
-      that.setData({
-        markers: wxMarkerData
-      });
-      that.setData({
-        latitude: wxMarkerData[0].latitude
-      });
-      that.setData({
-        longitude: wxMarkerData[0].longitude
-      });
-    }
-    // 发起POI检索请求 
-    BMap.search({
-      "query": '酒店',
-      fail: fail,
-      success: success,
-      // 此处需要在相应路径放置图片文件 
-      iconPath: '../../static/image/marker_red.png',
-      // 此处需要在相应路径放置图片文件 
-      iconTapPath: '../../static/image/marker_red.png'
-    });
   },
 
-  showSearchInfo: function (data, i) {
-    var that = this;
-    that.setData({
-      placeData: {
-        title: '名称：' + data[i].title + '\n',
-        address: '地址：' + data[i].address + '\n',
-        telephone: '电话：' + data[i].telephone
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function (e) {
+    this.mapCtx = wx.createMapContext('myMap')
+  },
+
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  getCenterLocation: function () {
+    this.mapCtx.getCenterLocation({
+      success: function (res) {
+        console.log(res.longitude)
+        console.log(res.latitude)
       }
-    });
+    })
   },
-  changeMarkerColor: function (data, i) {
-    var that = this;
-    var markers = [];
-    for (var j = 0; j < data.length; j++) {
-      if (j == i) {
-        // 此处需要在相应路径放置图片文件 
-        data[j].iconPath = "../../img/marker_yellow.png";
-      } else {
-        // 此处需要在相应路径放置图片文件 
-        data[j].iconPath = "../../img/marker_red.png";
+  moveToLocation: function () {
+    this.mapCtx.moveToLocation()
+  },
+
+  translateMarker: function () {
+    this.mapCtx.translateMarker({
+      markerId: 1,
+      autoRotate: true,
+      duration: 1000,
+      destination: {
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      },
+      animationEnd() {
+        console.log('animation end')
       }
-      markers[j](data[j]);
-    }
-    that.setData({
-      markers: markers
-    });
+    })
+  },
+
+  includePoints: function () {
+    this.mapCtx.includePoints({
+      padding: [10],
+      points: [{
+        latitude: 23.10229,
+        longitude: 113.3345211,
+      }, {
+        latitude: 23.00229,
+        longitude: 113.3345211,
+      }]
+    })
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
