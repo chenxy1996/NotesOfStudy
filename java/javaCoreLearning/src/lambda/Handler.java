9f13f63470d34219369cd14ba63768f2872f7f7e
Chen Xiangyu <chenxy19960202@gmail.com>
2019-09-28 23:43:37 +0800

    javaCore learning

--- java/javaCoreLearning/src/lambda/Handler.java
+++ java/javaCoreLearning/src/lambda/Handler.java
@@ -1,6 +1,5 @@
 package lambda;
 
 public interface Handler {
-    String name = "chen";
     double handle1(double x, double y, double z);
 }
--- java/javaCoreLearning/src/lambda/LambdaTest.java
+++ java/javaCoreLearning/src/lambda/LambdaTest.java
@@ -3,12 +3,18 @@ package lambda;
 
 import interfaceTest.Interface;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class LambdaTest {
     public static void main(String[] args) {
-        Handler callback = (a, b, c) -> a - b - c;
-        System.out.println(callback.handle1(3, 4, 5));
+        Handler callback = LambdaTest::add;
+        System.out.println(callback.handle1(1, 2, 3));
+        System.out.println(Handler.class);
+    }
+
+    public static double add(double a, double b, double c) {
+        return a + b + c;
     }
 }
