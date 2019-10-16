02c754d0aeddca359fea2addf8bfaebe016f63f4
Chen Xiangyu <chenxy19960202@gmail.com>
2019-10-16 23:50:38 +0800

    20191016

--- java/javaCoreLearning/src/concurrency/ThreadTest.java
+++ java/javaCoreLearning/src/concurrency/ThreadTest.java
@@ -1,5 +1,15 @@
 package concurrency;
 
 public class ThreadTest {
+    static int[] nums;
 
+    static {
+        for (int i = 0; i < 10000; i++) {
+            nums[i] = i;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
