0a421c24f5079596912161a151883bb6ba9a0cf7
Chen Xiangyu <chenxy19960202@gmail.com>
2019-10-21 23:05:41 +0800

    20191021

--- java/javaCoreLearning/src/concurrency/Bank.java
+++ java/javaCoreLearning/src/concurrency/Bank.java
@@ -1,28 +1,17 @@
 package concurrency;
 
-/*
-A lock object can have one or more asscociated condition objects. You obtain a condition
-object with the newCondition method. It is customary to give each condition object a name
-that evokes the condition that it represents. For example, here we set up a condition
-object to represent the "sufficient funds" condition.
-
-There is an essential difference between a thread that is waiting to acquire a lock and
-a thread that has called await. Once a thread calls the await method, it enters a wait
-set for that condition. The thread is not made runnable when the lock is available.
-Instead, it stays deactivated until another thread has called the signalAll method on
-the same condition.
- */
-
 import java.util.Collections;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
-    private static int MAX_ACCOUNT_NUMBER =10;
+    private static int MAX_ACCOUNT_NUMBER = 10;
     private double[] accounts;
     private static double MAX_AMOUNT = 100;
     private static int DELAY = 10;
+
+    private Object lock = new Object();
 
 //    private Lock bankLock;
 //    private Condition sufficientFunds;
@@ -60,24 +49,30 @@ public class Bank {
      * @param to the account to transfer to
      * @param amount the amount to transfer
      */
-    public synchronized void transfer(int from, int to, double amount) throws InterruptedException {
-        while (accounts[from] < amount) {
-            wait();
+    public  synchronized void transfer(int from, int to, double amount) throws InterruptedException {
+//        while (accounts[from] < amount) {
+//            wait();
+//        }
+//        System.out.print(Thread.currentThread());
+//        accounts[from] -= amount;
+//        System.out.printf(" %10.2f from %d to %d", amount, from, to);
+//        accounts[to] += amount;
+//        System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+//
+//        // check for the state of the lock condition.
+//        notifyAll();
+        synchronized (this) {
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
         }
-        System.out.print(Thread.currentThread());
-        accounts[from] -= amount;
-        System.out.printf(" %10.2f from %d to %d", amount, from, to);
-        accounts[to] += amount;
-        System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
-
-        // check for the state of the lock condition.
-        notifyAll();
     }
 
-    public static void main(String[] args) {
-        System.out.println("111");
+    public static void synchronizedTest1() {
         Bank bank = new Bank();
-        System.out.println("222");
+
         for (int i = 0; i < MAX_ACCOUNT_NUMBER; i++) {
             int fromAccount = i;
 
@@ -86,9 +81,7 @@ public class Bank {
                     while (true) {
                         int toAccount = (int) (bank.size() * Math.random());
                         double amount = MAX_AMOUNT * Math.random();
-                        if (bank.getBalance(fromAccount) >= amount) {
-                            bank.transfer(fromAccount, toAccount, amount);
-                        }
+                        bank.transfer(fromAccount, toAccount, amount);
                         Thread.sleep(DELAY);
                     }
                 } catch (InterruptedException e) {
@@ -99,5 +92,12 @@ public class Bank {
             Thread t = new Thread(r);
             t.start();
         }
+    }
+
+    public static void synchronizedTest2() {
+    }
+
+    public static void main(String[] args) {
+        synchronizedTest1();
     }
 }
